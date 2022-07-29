93c228241a2169fd5e9881cff19c4e737ea5dbc5
Vladislav <vk@gmail.com>
2022-07-29 19:56:15 +0300

    URL Params (useParams)

--- src/components/user.js
+++ src/components/user.js
@@ -1,7 +1,8 @@
 import React from "react"
-
+import { useParams } from "react-router-dom"
 const User = () => {
-  return <h2>User details</h2>
+  const { userId } = useParams()
+  return <h2>User details {userId}</h2>
 }
 
 export default User
