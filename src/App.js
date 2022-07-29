e40a6b39ac0d45c89d09503d5beb770f77bfe8d1
Your Name <you@example.com>
2022-07-29 13:09:21 +0300

    Navigating Programmatically ( hook useNavigate )

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import About from "./components/about"
 import Home from "./components/home"
 import Navbar from "./components/navbar"
+import OrderSummary from "./components/orderSummary"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
       </Routes>
     </>
   )
--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,14 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 const Home = () => {
-  return <h2>Home Page</h2>
+  const navigate = useNavigate()
+  return (
+    <>
+      <h2>Home Page</h2>
+      <button onClick={() => navigate("/order-summary")}>Order</button>
+    </>
+  )
 }
 
 export default Home
