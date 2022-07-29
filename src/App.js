f0685c7d4d7b9a12553d993f3650078c0ef462c2
Your Name <you@example.com>
2022-07-29 13:18:44 +0300

    No Match Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import About from "./components/about"
 import Home from "./components/home"
 import Navbar from "./components/navbar"
+import NoMatch from "./components/noMatch"
 import OrderSummary from "./components/orderSummary"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   )
